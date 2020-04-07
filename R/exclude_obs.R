b13d01533ae2fae07a784c387907f91b6094f137
graggsd <graggsd@uab.edu>
2020-04-07 14:34:49 -0500

    Added TODO item

--- R/exclude_obs.R
+++ R/exclude_obs.R
@@ -1,5 +1,6 @@
 #'@export
 exlude_obs <- function(data, exclusion_cb, id = "id") {
+    # TODO add documentation for exclude_obs
     stopifnot(id %in% colnames(data))
     stopifnot(id %in% colnames(exclusion_cb))
     excl_idx <- data[, id] %in% exclusion_cb[, id]
